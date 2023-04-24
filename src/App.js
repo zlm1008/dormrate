d1da4ac17832271795dec86f0076a8fdb5beda28
zlm1008 <zlm1008@wildcats.unh.edu>
2023-04-23 22:53:40 -0400

    h

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
+/* eslint-disable no-undef */
 import "./App.css";
-import { API } from "aws-amplify";
-import { Tabs, TabItem } from "@aws-amplify/ui-react";
+import { Tabs, TabItem, Button } from "@aws-amplify/ui-react";
 import { NewReview } from "./ui-components";
+import { Amplify, Auth } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
 
-let currUser = 'name';
-
-function getReviews(){
-
-}
+Amplify.configure({
+  Auth: {
+    userPoolId: "us-east-2_qM6RsOvr0",
+    userPoolWebClientId: "2tkvhvk5j1ktkpado1vhn6s7kt",
+    region: "us-east-2",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Tabs>
-        {/* Home Page*/}
-        <TabItem title="Home">
-          <header className="App-header">
-            <h2>Welcome {currUser}</h2>
-            {getReviews()}
-          </header>
-        </TabItem>
+    <Authenticator>
+      <div className="App">
+        <Tabs>
+          {/* Home Page*/}
+          <TabItem title="Home">
+            <header className="App-header">
+              <h2>Welcome {Auth.currentAuthenticatedUser}</h2>
+              {}
+            </header>
+          </TabItem>
 
-        {/* New Rating Page*/}
-        <TabItem title="New Review">
-          <header className="App-header">
-            <div class="card">
-              <h3 class="card-title mt-4">New Review</h3>
-              <hr></hr>
-              <div class="card-body">
-                <NewReview />
+          {/* New Rating Page*/}
+          <TabItem title="New Review">
+            <header className="App-header">
+              <div class="card">
+                <h3 class="card-title mt-4">New Review</h3>
+                <hr></hr>
+                <div class="card-body">
+                  <NewReview />
+                </div>
               </div>
-            </div>
-          </header>
-        </TabItem>
+            </header>
+          </TabItem>
 
-        {/* Account Page */}
-        <TabItem title="Account">
-          <header className="App-header">
-
-          </header>
-        </TabItem>
-      </Tabs>
-    </div>
+          {/* Account Page */}
+          <TabItem title="Sign Out" onClick={Amplify.signOut}>
+            <header className="App-header">
+              <Button onClick={Auth.signOut}>
+                Hello
+              </Button>
+            </header>
+          </TabItem>
+        </Tabs>
+      </div>
+    </Authenticator>
   );
 }
 
